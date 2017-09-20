c41879ae58a93e300658831ac35f36180a5330e9
Blaine Hodge <blaine@stingergames.com>
2017-09-20 13:45:04 -0700

    synchronized.cpp emulating java style synchornized ( obj ) { } code block

---
+++ synchronized.cpp
@@ -0,0 +1,38 @@
+#include <mutex>
+
+#include <stdio.h>
+
+#define synchronized(x) if ( synchronized_helper __helper{x} )
+
+class synchronized_helper {
+
+	std::mutex & obj;
+
+public:
+
+	synchronized_helper(std::mutex & _obj) : obj( _obj ) {
+		printf( "synchronized_helper lock\n" );
+		obj.lock();
+	}
+
+	~synchronized_helper() {
+		printf( "synchronized_helper unlock\n" );
+		obj.unlock();
+	}
+
+	operator bool() { return true; }
+
+private:
+
+	synchronized_helper(const synchronized_helper & other);
+	synchronized_helper & operator =(const synchronized_helper & other);
+
+};
+
+int main() {
+	std::mutex m;
+	synchronized ( m ) {
+		printf( "in synchronized\n" );
+	}
+	return 0;
+}
